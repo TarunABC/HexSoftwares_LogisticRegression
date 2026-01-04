cf9dc8d54aeba8d6635ebfc27702b63f361ce0d9
Tarun N <tarunnk2023@gmail.com>
2026-01-04 15:51:26 +0530

    Add logistic regression model for heart disease prediction

---
+++ model.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+# Load dataset
+df = pd.read_csv("C:/Users/Admin/Downloads/heart_disease_data.csv")
+
+# Preview dataset
+print(df.head())
